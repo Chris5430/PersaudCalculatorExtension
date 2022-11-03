4b1b6d0c7367821193ebb3287b3331028efc1899
Christopher Persaud <christopherp543@nycstudents.net>
2022-11-03 12:10:44 -0400

    commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+
+    public Calculator(){
+
+    }
+
+    public double add(double x, double y){
+        return x + y;
+    }
+
+    public double subtract(double x, double y){
+        return x - y;
+    }
+
+    public double multiply(double x, double y){
+        return x * y;
+    }
+
+    public double divide(double x, double y){
+        return x / y;
+    }
+
+    public double mod(double x, double y){
+        return x % y;
+    }
+
+    public double power(double x, double y){
+        return Math.pow(x, y);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Calculator calc = new Calculator();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an expression with *, /, +, -, %, or ^: ");
+        String expression = input.nextLine();
+        if(expression.indexOf("+") != -1){
+            double num1 = Double.parseDouble(expression.substring(0, expression.indexOf("+") - 1));
+            double num2 = Double.parseDouble(expression.substring(expression.indexOf("+") + 1));
+            System.out.println(calc.add(num1, num2));
+        }
+        if(expression.indexOf("-") != -1){
+            double num1 = Double.parseDouble(expression.substring(0, expression.indexOf("-") - 1));
+            double num2 = Double.parseDouble(expression.substring(expression.indexOf("-") + 1));
+            System.out.println(calc.subtract(num1, num2));
+        }
+        if(expression.indexOf("*") != -1){
+            double num1 = Double.parseDouble(expression.substring(0, expression.indexOf("*") - 1));
+            double num2 = Double.parseDouble(expression.substring(expression.indexOf("*") + 1));
+            System.out.println(calc.multiply(num1, num2));
+        }
+        if(expression.indexOf("/") != -1){
+            double num1 = Double.parseDouble(expression.substring(0, expression.indexOf("/") - 1));
+            double num2 = Double.parseDouble(expression.substring(expression.indexOf("/") + 1));
+            System.out.println(calc.divide(num1, num2));
+        }
+        if(expression.indexOf("%") != -1){
+            double num1 = Double.parseDouble(expression.substring(0, expression.indexOf("%") - 1));
+            double num2 = Double.parseDouble(expression.substring(expression.indexOf("%") + 1));
+            System.out.println(calc.mod(num1, num2));
+        }
+        if(expression.indexOf("^") != -1){
+            double num1 = Double.parseDouble(expression.substring(0, expression.indexOf("^") - 1));
+            double num2 = Double.parseDouble(expression.substring(expression.indexOf("^") + 1));
+            System.out.println(calc.power(num1, num2));
+        }
+    }
 }
